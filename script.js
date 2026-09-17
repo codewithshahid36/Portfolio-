4611fd1517de8efbafd548cacdb66d5fead894d3
Shahid-git091 <hussainshahid83215@gmail.com>
2026-09-17 19:53:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Portfolio loaded successfully");
+
+function showMessage() {
+    alert("Welcome to my portfolio!");
+}
+document.getElementById("year").textContent=new Date().getFullYear();
